5d28b6b4074713c8ce359c7a6646636c598e4d9b
Tanya <tarja_1989@list.ru>
2022-12-15 02:41:39 +0300

    Init

---
+++ src/main/java/com/example/vinylstore/controllers/FrontController.java
@@ -0,0 +1,2 @@
+package com.example.vinylstore.controllers;public class FrontController {
+}
---
+++ src/test/java/com/example/vinylstore/VinylStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vinylstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
